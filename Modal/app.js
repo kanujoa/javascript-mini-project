1130a033bc17cc2ef3220de60917019313a37a3c
yubeenso <youbin0105@gmail.com>
2023-03-17 11:52:18 +0900

    modal

---
+++ Modal/app.js
@@ -0,0 +1,27 @@
+const modalBtn = document.querySelector(".modal-btn");
+const modalOverlay = document.querySelector(".modal-overlay");
+modalBtn.addEventListener("click", function() {
+  modalOverlay.classList.add("open-modal");
+});
+
+const closeBtn = document.querySelector(".close-btn");
+closeBtn.addEventListener("click", function() {
+  modalOverlay.classList.remove("open-modal");
+})
+
+
+// select modal-btn,modal-overlay,close-btn
+// listen for click events on modal-btn and close-btn
+// when user clicks modal-btn add .open-modal to modal-overlay
+// when user clicks close-btn remove .open-modal from modal-overlay
+
+// const modalBtn = document.querySelector(".modal-btn");
+// const modal = document.querySelector(".modal-overlay");
+// const closeBtn = document.querySelector(".close-btn");
+
+// modalBtn.addEventListener("click", function () {
+//   modal.classList.add("open-modal");
+// });
+// closeBtn.addEventListener("click", function () {
+//   modal.classList.remove("open-modal");
+// });
